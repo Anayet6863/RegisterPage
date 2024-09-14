686073301e2a08faaaf232577199489843ea7243
anayet6866 <mdsaadahmed6866@gmail.com>
2024-09-15 01:50:38 +0600

    This is a simple Register page which is implemented by using html and css

---
+++ hablu.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    // let call = document.getElementById("signin-call");
+    let here = document.getElementById("signin-show");
+    const p = document.createElement("p");
+    p.innerText = "Enjoy your journey.";
+    here.appendChild(p);
+    // alert("Enjoy your journey.");
+}
+
+
