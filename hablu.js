f110ef99a4ff7d042e8428e971c289241fe11d33
Md Anayet Ullah <109759412+Anayet6863@users.noreply.github.com>
2024-09-16 13:55:05 +0600

    Add javascript functionality

--- hablu.js
+++ hablu.js
@@ -1,10 +1,13 @@
-function myFunction() {
-    // let call = document.getElementById("signin-call");
-    let here = document.getElementById("signin-show");
-    const p = document.createElement("p");
-    p.innerText = "Enjoy your journey.";
-    here.appendChild(p);
-    // alert("Enjoy your journey.");
-}
+// function myFunction() {
+//     // let call = document.getElementById("signin-call");
+//     let here = document.getElementById("signin-show");
+//     const p = document.createElement("p");
+//     p.innerText = "Enjoy your journey.";
+//     here.appendChild(p);
+//     // alert("Enjoy your journey.");
+// }
 
-
+document.getElementById('signin-call').addEventListener('click',function(){
+    const call = document.getElementById('signin-show');
+    call.innerHTML = "Enjoy your journey."+new Date();
+})
